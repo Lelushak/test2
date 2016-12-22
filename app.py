5728af53dce71712fee1bf55fbf434a58589b651
Bananas-power <Bananas-power@users.noreply.github.com>
2016-12-22 23:59:41 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import config
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(commands=['start', 'help'])
+def handle_start_help(message):
+    bot.send_message(message.chat.id, "Hello"
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): 
+    bot.send_message(message.chat.id, message.text)
+    
+@bot.message_handler(commands=["geophone"])
+def geophone(message):
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    button_phone = types.KeyboardButton(text="Отправить номер телефона", request_contact=True)
+    button_geo = types.KeyboardButton(text="Отправить местоположение", request_location=True)
+    keyboard.add(button_phone, button_geo)
+    bot.send_message(message.chat.id, "Отправь мне свой номер телефона или поделись местоположением, жалкий человечишка!", reply_markup=keyboard)
+
+@bot.message_handler(commands=["testkey"])
+def ReplyKeyboardHide()     
+    markup = types.ReplyKeyboardMarkup()
+    markup.row('Numerics', 'Symbols')
+    markup.row('Special')
+    bot.send_message(message.chat.id, "Choose:", reply_markup=markup)
+
+
+
+if __name__ == '__main__':
+     bot.polling(none_stop=True)	
